5a1e00a730fb6b8754ed1108c21840765d967588
rajeswarisakthi <rajeswarisakthivel1307@gmail.com>
2025-09-29 10:09:03 +0530

    Add files via upload

---
+++ program.22.py
@@ -0,0 +1,7 @@
+print("Triangle pattern")
+print("------------------")
+n=int(input("Enter the number:"))
+for i in range(1, n+1):
+            for j in range(i):
+                print("*",end="")
+            print()
