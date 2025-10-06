ebafd52bf87ae01ec5515415eb7cf1e50c95b3f7
rajeswarisakthi <rajeswarisakthivel1307@gmail.com>
2025-10-06 10:24:34 +0530

    program.py25

---
+++ program.25.py
@@ -0,0 +1,10 @@
+print("Left Arrow Pattern")
+print("------------------")
+
+n = int(input("Enter the number of rows: "))
+
+for i in range(1, n + 1):
+    print("  " * (n - i) + "* " * i)
+          
+for i in range(n - 1, 0, -1):
+    print("  " * (n - i) + "* " * i)
